950d7f1400d8f524ce0c16268b373265778e4fad
Catherine Yuan <catherine.cj.yuan@gmail.com>
2020-06-11 23:03:01 -0400

    initial playground and testing environ setup

---
+++ test/src/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @BeforeEach
+    public void setUp() {
+        // do something
+    }
+
+    @Test
+    public void testingSomeMethod() {
+        // do something
+    }
+}
